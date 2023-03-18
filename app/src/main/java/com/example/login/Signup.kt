1441e769d3363984b146c14e9a33c323f2af3463
mesh-dell <mwaimonicas@gmail.com>
2023-03-19 00:24:50 +0300

    sign up functionality

--- app/src/main/java/com/example/login/Signup.kt
+++ app/src/main/java/com/example/login/Signup.kt
@@ -1,11 +1,49 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
+private lateinit var etPassword: EditText
+private lateinit var etEmail: EditText
+private lateinit var etName: EditText
+private lateinit var auth: FirebaseAuth
+private lateinit var btnSignup: Button
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        etPassword = findViewById(R.id.et_password)
+        etEmail = findViewById(R.id.et_email)
+        etName = findViewById(R.id.et_name)
+        btnSignup = findViewById(R.id.btnSignup)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        btnSignup.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            signUp(email, password)
+        }
+    }
+
+    private fun signUp(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity2::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
